a166d8b64e138b48806613888ad5a8ff8b2dff75
monmartius <monmarius@gmail.com>
2019-08-13 19:27:20 +0700

    First commit 13:51

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlite:/d/projects/test-project/data/base.db',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
